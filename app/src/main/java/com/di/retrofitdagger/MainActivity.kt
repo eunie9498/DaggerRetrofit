fe79a23e37e1581986f1185557043cf1ac14c92b
eunie9498 <bvegemilb@naver.com>
2022-03-20 17:44:22 +0900

    add library

---
+++ app/src/main/java/com/di/retrofitdagger/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.di.retrofitdagger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //https://run.mocky.io/v3/345bfd6f-97e3-4e41-8113-b9584f571efc
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
